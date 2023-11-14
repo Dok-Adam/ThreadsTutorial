119e1e87e460e58f89b8c2e43d3ec2c22cfb40d5
Adam <doktoradam72@gmail.com>
2023-11-14 17:13:09 +0500

    fix bugs

---
+++ Threads/Core/Profile/View/CurrentUserProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  CurrentUserProfileView.swift
+//  Threads
+//
+//  Created by Адам Дотмерзаев on 14.11.2023.
+//
+
+import SwiftUI
+
+struct CurrentUserProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CurrentUserProfileView()
+}
