23de7545471876e8561bf6e1166067c1019ab778
Adam <doktoradam72@gmail.com>
2023-11-15 10:42:59 +0500

    Add views

---
+++ Threads/Core/Profile/Components/ProfileHeaderView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileHeaderView.swift
+//  Threads
+//
+//  Created by Адам Дотмерзаев on 15.11.2023.
+//
+
+import SwiftUI
+
+struct ProfileHeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileHeaderView()
+}
---
+++ Threads/Core/Profile/Components/UserContentListView.swift
@@ -0,0 +1,18 @@
+//
+//  UserContentListView.swift
+//  Threads
+//
+//  Created by Адам Дотмерзаев on 15.11.2023.
+//
+
+import SwiftUI
+
+struct UserContentListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserContentListView()
+}
