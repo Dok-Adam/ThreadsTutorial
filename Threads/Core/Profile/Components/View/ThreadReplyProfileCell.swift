daf4f89b53144838ceebe45efe8c5ace254fb85f
Adam <doktoradam72@gmail.com>
2023-11-17 10:15:47 +0500

    add new view

---
+++ Threads/Core/Profile/Components/View/ThreadReplyProfileCell.swift
@@ -0,0 +1,18 @@
+//
+//  ThreadReplyProfileCell.swift
+//  Threads
+//
+//  Created by Адам Дотмерзаев on 17.11.2023.
+//
+
+import SwiftUI
+
+struct ThreadReplyProfileCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ThreadReplyProfileCell()
+}
